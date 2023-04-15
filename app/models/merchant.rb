3fe8f77c69b9c986df43e18106452a915f1859e5
Katie Lonsdale <katie952@gmail.com>
2023-04-15 15:35:28 -0600

    refactor: items_ready method to sort in reverse

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -14,6 +14,6 @@ class Merchant < ApplicationRecord
   end
 
   def items_ready
-    invoice_items.where.not(status: 2).distinct
+    invoice_items.where.not(status: 2).distinct.order(created_at: :desc)
   end
 end
