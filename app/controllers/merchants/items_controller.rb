822bd22a75a8ebfad2bfa73513e9dd8205e59aca
Crow Rising <miscbyrd@gmail.com>
2023-04-15 11:51:42 -0600

    style: Add view to merchant items index

--- app/controllers/merchants/items_controller.rb
+++ app/controllers/merchants/items_controller.rb
@@ -1,6 +1,7 @@
 class Merchants::ItemsController < ApplicationController
 
   def index
-    
+    @merchant = Merchant.find(params[:id])
+    @items = @merchant.items
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -11,5 +11,4 @@ Rails.application.routes.draw do
  get '/merchants/:id/dashboard', to: 'merchants#show'
  get '/merchants/:id/items', to: 'merchants/items#index'
  get '/merchants/:id/invoices', to: 'merchants/invoices#index'
-
 end
