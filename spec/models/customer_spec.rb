7763995d93960f613a5e5bb596457ca84dbdd0dd
Katie Lonsdale <katie952@gmail.com>
2023-04-16 14:09:13 -0600

    refactor: remove transaction_count test

--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -104,17 +104,5 @@ RSpec.describe Customer do
         expect(@cust_1.full_name).to eq("#{@cust_1.first_name} #{@cust_1.last_name}")
       end
     end
-
-    describe '#transaction_count' do
-      it 'returns the count of all successful transactions for a merchant by customer' do
-        delete_data
-
-        us_3_test_data
-
-        expect(@cust_6.transaction_count(@merch_1)).to eq(6)
-        expect(@cust_3.transaction_count(@merch_1)).to eq(4)
-        expect(@cust_5.transaction_count(@merch_1)).to eq(2)
-      end
-    end
   end
 end
