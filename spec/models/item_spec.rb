996e3704fd49d553d2630e41f2a974d093553b26
Katie Lonsdale <katie952@gmail.com>
2023-04-17 15:20:50 -0600

    test: remove test for removed method

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -9,13 +9,4 @@ RSpec.describe Item do
     it { should have_many(:invoice_items).dependent(:destroy) }
     it { should have_many(:invoices).through(:invoice_items) }
   end
-
-  describe 'class methods' do
-    describe "::found_on_invoice" do
-      it "returns all items found on given invoice for given merchant" do
-        us_16_test_data
-        expect(Item.found_on_invoice(@merch_1, @invoice_1)).to eq([@item_1, @item_2])
-      end
-    end
-  end
 end
