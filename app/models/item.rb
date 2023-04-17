3ddeade22105420f67bd881f42b4fd2990d68afc
Katie Lonsdale <katie952@gmail.com>
2023-04-17 13:11:09 -0600

    feat: add found_on_invoice method

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,4 +2,10 @@ class Item < ApplicationRecord
   belongs_to :merchant
   has_many :invoice_items, dependent: :destroy
   has_many :invoices, through: :invoice_items
+
+  def self.found_on_invoice(merchant, invoice)
+    joins(:merchant, :invoices)
+    .where("merchants.id=? AND invoices.id=?", merchant, invoice)
+    .distinct
+  end
 end
