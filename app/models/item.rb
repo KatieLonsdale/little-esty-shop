19f4a6acb495f991f1727366a042134893617457
Katie Lonsdale <katie952@gmail.com>
2023-04-19 12:30:04 -0600

    feat: add opposite_status method

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,4 +3,8 @@ class Item < ApplicationRecord
   has_many :invoice_items, dependent: :destroy
   has_many :invoices, through: :invoice_items
   enum status: { disabled: 0, enabled: 1 }
+
+  def opposite_status
+    enabled? ? 'Disable' : 'Enable'
+  end
 end
