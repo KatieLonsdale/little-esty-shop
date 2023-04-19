5136edb916d9f27817e0af22863bea5eba47b344
Crow Rising <miscbyrd@gmail.com>
2023-04-19 17:17:31 -0600

    final commit for user story 11

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,9 @@ Rails.application.routes.draw do
     resources :merchants, only: [:index, :show, :edit, :update, :new, :create]
     resources :invoices, only: [:index, :show]
   end
-
+  
+ get '/merchants/:merchant_id/items/new', to: 'merchants/items#new'
+ post '/merchants/:merchant_id/items', to: 'merchants/items#create'
  get '/merchants/:merchant_id/items/:item_id', to: 'merchants/items#show'
  get '/merchants/:merchant_id/items/:item_id/edit', to: 'merchants/items#edit'
  patch '/merchants/:merchant_id/items/:item_id', to: 'merchants/items#update'
@@ -15,6 +17,5 @@ Rails.application.routes.draw do
  get '/merchants/:id/items', to: 'merchants/items#index'
  get '/merchants/:id/invoices', to: 'merchants/invoices#index'
  get '/merchants/:merchant_id/invoices/:invoice_id', to: 'merchants/invoices#show'
-
  patch '/invoice_items/:id', to: 'invoice_items#update'
 end
---
+++ spec/features/merchants/items/new_spec.rb
@@ -0,0 +1,29 @@
+
+require 'rails_helper'
+require './spec/testable.rb'
+
+include Testable
+
+RSpec.describe 'merchant items new page' do
+  before(:all) do
+    delete_data
+    @merch_2 = create(:merchant)
+    visit "/merchants/#{@merch_2.id}/items/new" 
+  end
+  describe 'when I visit my my items new page' do
+    it 'displays a form where I can add new item and item info' do
+
+      within("#new-item-info") do  
+
+        fill_in('name', with: 'ceramic watering can')
+        fill_in('description', with: 'heavy and impractical')
+        fill_in('unit_price', with: 49)
+        save_and_open_page
+        click_on("Submit")
+      end
+
+      expect(current_path).to eq("/merchants/#{@merch_2.id}/items")
+      expect(page).to have_content('ceramic watering can')
+    end
+  end
+end
