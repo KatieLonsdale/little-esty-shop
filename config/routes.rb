d3552874bded17f1e5684119f502adb66fdaa6f2
Boston Lowrey <bostonlowrey@gmail.com>
2023-04-19 15:53:22 -0400

    feat: add create route for admin/merchants

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   resources :admin, only: [:index]
 
   namespace :admin do
-    resources :merchants, only: [:index, :show, :edit, :update, :new]
+    resources :merchants, only: [:index, :show, :edit, :update, :new, :create]
     resources :invoices, only: [:index]
   end
 
