042e9125a533fb307ca9c75b63f0c7c451431a96
Crow Rising <miscbyrd@gmail.com>
2023-04-19 14:21:45 -0600

    finish user story 8

--- spec/features/merchants/items/show_spec.rb
+++ spec/features/merchants/items/show_spec.rb
@@ -15,24 +15,23 @@ RSpec.describe 'merchant items show page' do
 
     it 'displays a link to update item information and am directed to a form to update item' do
       visit "/merchants/#{@merch_2.id}/items/#{@item_2.id}"
-   
+      
       within("#update-item-info") do
-       
         click_link("Update Item")
-
-        expect(current_path).to eq("/merchants/#{@merch_2.id}/items/#{@item_2.id}/edit")
       end
-
-        visit "/merchants/#{@merch_2.id}/items/#{@item_2.id}"
-
+      
+      expect(current_path).to eq("/merchants/#{@merch_2.id}/items/#{@item_2.id}/edit")
+      
+      within("#update-info") do  
         fill_in('description', with: 'lovely and useful')
         fill_in('unit_price', with: 175)
-
+        
         click_on("Submit")
-
-        expect(current_path).to eq("/merchants/#{@merch_2.id}/items/#{@item_2.id}")
-        expect(page).to have_content('lovely and useful')
-        expect(page).to have_content(175)
+      end
+        
+      expect(current_path).to eq("/merchants/#{@merch_2.id}/items/#{@item_2.id}")
+      expect(page).to have_content('lovely and useful')
+      expect(page).to have_content(175)
     end
   end
 end
